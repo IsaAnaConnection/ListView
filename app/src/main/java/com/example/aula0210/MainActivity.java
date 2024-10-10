42c13c371922dd6505c2f3bd5043f689b42d6144
AnnaDays <analuizadias389@gmail.com>
2024-10-10 10:41:07 -0300

    Alterações - Tela de CRUD

--- app/src/main/java/com/example/aula0210/MainActivity.java
+++ app/src/main/java/com/example/aula0210/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.aula0210;
 
-import android.content.Intent;
+import android.content.Intent;  // Importar a classe Intent
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,8 +18,10 @@ import com.example.aula0210.dao.AlunoDAO;
 import com.example.aula0210.model.Aluno;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnListar, btnNovo, btnAlterar, btnExcluir, btnSalvar, btnConsultar;
-    EditText edtId, edtNome, edtCpf, edtTelefone, edtListar;
+    Button btnNovo, btnAlterar, btnExcluir, btnSalvar, btnConsultar;
+    EditText edtId, edtNome, edtCpf, edtTelefone;
+    TextView txtSair; // Declare txtSair
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,64 +32,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Inicialize os componentes
         edtNome = findViewById(R.id.edtNome);
         edtId = findViewById(R.id.edtId);
         edtCpf = findViewById(R.id.edtCpf);
-        edtTelefone= findViewById(R.id.edtTelefone);
-        edtListar = findViewById(R.id.edtListar);
+        edtTelefone = findViewById(R.id.edtTelefone);
+        txtSair = findViewById(R.id.txtSair); // Inicialize o txtSair
 
-        Intent intent = getIntent();
-        if (intent != null) {
-            int id = intent.getIntExtra("id", -1);
-            String nome = intent.getStringExtra("nome");
-            String cpf = intent.getStringExtra("cpf");
-            String telefone = intent.getStringExtra("telefone");
-
-            // Preenche os campos se o ID for válido
-            if (id != -1) {
-                edtId.setText(String.valueOf(id));
-                edtNome.setText(nome);
-                edtCpf.setText(cpf);
-                edtTelefone.setText(telefone);
-            }
-        }
-
+        // Defina o listener para o txtSair
+        txtSair.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, TelaLista.class);
+            startActivity(intent);
+        });
     }
-    public void novo (View view){
+
+    public void novo(View view) {
         edtNome.setText(null);
         edtId.setText(null);
         edtCpf.setText(null);
         edtTelefone.setText(null);
-        edtListar.setText(null);
-
     }
-    public void salvar (View view){
+
+    public void salvar(View view) {
         Aluno a = new Aluno();
         a.setNome(edtNome.getText().toString());
         a.setCpf(edtCpf.getText().toString());
         a.setTelefone(edtTelefone.getText().toString());
         AlunoDAO dao = new AlunoDAO(this);
         long id = dao.insert(a);
-        Toast.makeText(getApplicationContext(), "Aluno inserido com o ID "+id,
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Aluno inserido com o ID " + id, Toast.LENGTH_LONG).show();
     }
 
-    public void consultar (View view){
+    public void consultar(View view) {
         AlunoDAO dao = new AlunoDAO(this);
         Aluno a = dao.read(Integer.parseInt(edtId.getText().toString()));
         edtNome.setText(a.getNome());
         edtCpf.setText(a.getCpf());
         edtTelefone.setText(a.getTelefone());
     }
-    public void excluir(View view){
+
+    public void excluir(View view) {
         Aluno a = new Aluno();
         a.setId(Integer.parseInt(edtId.getText().toString()));
         AlunoDAO dao = new AlunoDAO(this);
         dao.delete(a);
-        Toast.makeText(getApplicationContext(), "Aluno Excluido!",
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Aluno Excluído!", Toast.LENGTH_LONG).show();
     }
-    public void alterar(View view){
+
+    public void alterar(View view) {
         Aluno a = new Aluno();
         a.setId(Integer.parseInt(edtId.getText().toString()));
         a.setNome(edtNome.getText().toString());
@@ -94,8 +88,6 @@ public class MainActivity extends AppCompatActivity {
         a.setTelefone(edtTelefone.getText().toString());
         AlunoDAO dao = new AlunoDAO(this);
         dao.update(a);
-        Toast.makeText(getApplicationContext(), "Aluno alterado! ",
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Aluno alterado!", Toast.LENGTH_LONG).show();
     }
-
 }
